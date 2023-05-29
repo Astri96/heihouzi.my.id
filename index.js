ac3d3d0ed63d4405e61fe73c95eea352f44b43ca
Astrid96 <yogaastri0902@gmail.com>
2023-05-29 13:25:28 +0700

    add changelog

--- index.js
+++ index.js
@@ -5,5 +5,30 @@ app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.status(404)
   res.sendFile(__dirname + "/404.html")
 });
+app.use(express.static(__dirname + "/public/tugasppl"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/tugasppl/index.html")
+});
+
+app.use(express.static(__dirname + "/public/changelog"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/changelog/index.html")
+});
+app.use(express.static(__dirname + "/public/bot"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/bot/index.html")
+});
+app.use(express.static(__dirname + "/public/discord"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/discord/index.html")
+});
+app.use(express.static(__dirname + "/public/whatsapp"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/whatsapp/index.html")
+});
+app.use(express.static(__dirname + "/public/linktree"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/linktree/index.html")
+});
+app.use(express.static(__dirname + "/public/instagram"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/instagram/index.html")
+});
+app.use(express.static(__dirname + "/public/giveaway"), (_, res, next) => {
+  res.sendFile(__dirname + "./public/giveaway/index.html")
+});
 
 app.listen(8080);
